fa5d4232086b161d3089c156217eba1b42ac0ec9
gomandev <gomandev@gmail.com>
2020-07-30 12:18:18 +0100

    fashion-design

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,31 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
-import img from '../../images/have.jpg'
+import { ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+const LAUNCH_DATA = gql`
+  query LaunchData {
+    launches {
+      flight_number
+      mission_name
+      launch_success
+      launch_date_local
+    }
+  }
+`;
 
 const Project = () => {
-    return (
-        <>
-            <Wrapper>
-                <Container>
-                    <HaveAproject style={{ backgroundImage: `url(${img})` }}>
-                        <div class="row d-flex align-items-center">
-                            <div class="col-xl-7 col-lg-9 col-md-12">
-                                <Work>
-                                    <h2>Have project in mind?</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut.</p>
-                                </Work>
-                            </div>
-                            <div class="col-xl-5 col-lg-3 col-md-12">
-                                <Btn class="f-right">
-                                    <a href="#" class="btn btn-ans">Contact Us</a>
-                                </Btn>
-                            </div>
-                        </div>
-                    </HaveAproject>
-                </Container>
-            </Wrapper>
-        </>
-    )
+  const { loading, error, data } = useQuery(LAUNCH_DATA);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log(data)
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Wrapper>
+          <Container>
+            <h2></h2>
+          </Container>
+        </Wrapper>
+      </ApolloProvider>
+    </>
+  )
 }
 
 export const Container = styled.div`
@@ -33,100 +41,6 @@ export const Container = styled.div`
   margin: auto;
   overflow: hidden;
    
-`
-
-export const HaveAproject = styled.div`
-    padding:64px 69px 54px 78px;
-    background-repeat:no-repeat;
-    border-radius:10px;
-    position:relative;
-  
-  @media only screen and (min-width: 768px) and (max-width: 991px){
-      
-    padding:42px 69px 36px 57px;
-    
-  }
-  @media only screen and (min-width: 576px) and (max-width: 767px){
-      
-    padding:42px 19px 36px 40px;
-  }
-  @media (max-width: 767px){
-      
-    padding:42px 19px 36px 40px;
-  }
-    &:before{
-        position:absolute;
-        width:100%;
-        height:100%;
-        bottom:0;
-        left:0;
-        content:"";
-        border-radius:10px;
-        opacity:0.95;
-        background-image:-moz-linear-gradient(0deg, #ff5c98 0%, #7b40c0 100%);
-        background-image:-webkit-linear-gradient(0deg, #ff5c98 0%, #7b40c0 100%);
-        background-image:-ms-linear-gradient(0deg, #ff5c98 0%, #7b40c0 100%)
-    }
-`
-
-export const Work = styled.div`
-    h2{
-        color:#fff;
-        font-size:40px;
-        font-weight:700;
-        line-height:1.4;
-        margin-bottom:13px;
-    }
-    @media only screen and (min-width: 576px) and (max-width: 767px){
-        h2{
-            padding-right:20px;
-            margin-bottom:18px;
-        }
-    }
-    @media (max-width: 767px){
-        h2{
-            margin-bottom:18px;
-            font-size:31px;
-        }
-    }
-    p{
-        color:#fff;
-    }
-    @media only screen and (min-width: 992px) and (max-width: 1199px){
-        p{
-            padding-right:20px;
-        }
-    }
-    @media only screen and (min-width: 768px) and (max-width: 991px){
-        p{
-            padding-right:20px;
-            margin-bottom:30px;
-        }
-    }
-    @media only screen and (min-width: 576px) and (max-width: 767px){
-       p{
-           margin-bottom:30px;
-        }
-    }
-    @media (max-width: 767px){
-        p{
-            margin-bottom:30px;
-            font-size:15px;
-        }
-    }
-    
-`
-
-export const Btn = styled.div`
-    @media only screen and (min-width: 768px) and (max-width: 991px){
-        float:left;
-    }
-    @media only screen and (min-width: 576px) and (max-width: 767px){
-        float:left;
-    }
-    @media (max-width: 767px){
-        float:left;
-    }
 `
 
 export const Wrapper = styled.div`
