c283a8b2d7cbf1868b85e9d9efba516e880de8c5
gomandev <gomandev@gmail.com>
2020-07-30 12:49:17 +0100

    returning the client nto original

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,34 @@ import React from "react"
 import SEO from "../components/seo"
 import '../components/style.css'
 import Project from '../components/Project'
+import { ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery } from "@apollo/client"
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+const LAUNCH_DATA = gql`
+  query LaunchData {
+    launches {
+      flight_number
+      mission_name
+      launch_success
+      launch_date_local
+    }
+  }
+`;
 const IndexPage = () => {
+  const { loading, error, data } = useQuery(LAUNCH_DATA);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log(data)
   return (
   
-    <>
+    <ApolloProvider client={client}>
       <SEO title="Home" />
       <Project />
-    </>
+    </ApolloProvider>
   )
 } 
 
